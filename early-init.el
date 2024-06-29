a0faabda68eff18d6f2ea287dfbd77f100556c17
k <zrignshw@duck.com>
2024-06-30 04:18:00 +0800

    add meow

--- early-init.el
+++ early-init.el
@@ -5,7 +5,7 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-;;(push '(alpha-background . 80) default-frame-alist)
+(push '(alpha-background . 80) default-frame-alist)
 
 ;;; end
 
