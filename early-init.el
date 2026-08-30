c1a9eb591713f112edef2034e666a29e8b868e70
keke <keke@local>
2026-08-30 13:46:40 +0000

    Adapt configuration for Emacs 31
    Require lexical-binding, set elpaca-core-date, use built-in
    tree-sitter auto-install and lua-ts-mode, and tidy early-init.

--- early-init.el
+++ early-init.el
@@ -1,16 +1,18 @@
-;;; user interface
+;;; early-init.el --- Early initialization -*- lexical-binding: t; -*-
+
+;; Emacs 31: site-start.el now loads *before* early-init.el.
+;; package.el 必须在这里关掉，Elpaca 才不会和它抢启动。
 
 (setq package-enable-at-startup nil)
 (setq inhibit-splash-screen t)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(push '(alpha-background . 80) default-frame-alist)
 
-;;; end
-
-
-;;; font
+(setq default-frame-alist
+      (append
+       '((menu-bar-lines . 0)
+         (tool-bar-lines . 0)
+         (vertical-scroll-bars . nil)
+         (alpha-background . 80))
+       default-frame-alist))
 
 (defvar font-list
   (cond
@@ -20,21 +22,21 @@
     '(("Iosevka Term Curly" . 14) ("Consolas" . 12) ("Cascadia Mono" . 11)))
    (t
     '(("MonoLisa Nasy" . 11) ("SF Mono" . 11) ("Consolas" . 12))))
-  "List of fonts and sizes.  The first one available will be used.")
+  "Fonts to try.  First available entry is used for the default face.")
 
-(add-to-list 'default-frame-alist (cons 'font (format "%s-%d" (caar font-list) (cdar font-list))))
-
+(add-to-list 'default-frame-alist
+             (cons 'font (format "%s-%d" (caar font-list) (cdar font-list))))
 (set-fontset-font t 'unicode "Iosevka Term Curly")
-;;; end
 
+;; system-type 在 macOS 上是 darwin，没有 'macos。
+(when (eq system-type 'darwin)
+  (add-to-list 'default-frame-alist '(alpha . 80))
+  (condition-case err
+      (let ((path (with-temp-buffer
+                    (insert-file-contents-literally "~/.path")
+                    (buffer-string))))
+        (setenv "PATH" path)
+        (setq exec-path (append (parse-colon-path path) (list exec-directory))))
+    (error (warn "%s" (error-message-string err)))))
 
-(if (or (eq system-type 'darwin) (eq system-type 'macos))
-    (progn
-      (push '(alpha . 80) default-frame-alist)
-      (condition-case err
-	  (let ((path (with-temp-buffer
-			(insert-file-contents-literally "~/.path")
-			(buffer-string))))
-	    (setenv "PATH" path)
-	    (setq exec-path (append (parse-colon-path path) (list exec-directory))))
-	(error (warn "%s" (error-message-string err))))))
+;;; early-init.el ends here
